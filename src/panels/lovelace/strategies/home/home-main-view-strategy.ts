ff28ca11c9d120ed8a16150bd191913f747ae1b8
Paul Bottein <paul.bottein@gmail.com>
2025-11-25 11:50:00 +0100

    Add unassigned devices view to home dashboard

--- src/panels/lovelace/strategies/home/home-main-view-strategy.ts
+++ src/panels/lovelace/strategies/home/home-main-view-strategy.ts
@@ -228,6 +228,19 @@ export class HomeMainViewStrategy extends ReactiveElement {
             columns: 4,
           },
         } satisfies HomeSummaryCard),
+      {
+        type: "home-summary",
+        summary: "unassigned_devices",
+        vertical: true,
+        tap_action: {
+          action: "navigate",
+          navigation_path: "unassigned-devices",
+        },
+        grid_options: {
+          rows: 2,
+          columns: 4,
+        },
+      } satisfies HomeSummaryCard,
     ].filter(Boolean) as LovelaceCardConfig[];
 
     const summarySection: LovelaceSectionConfig = {
@@ -295,6 +308,29 @@ export class HomeMainViewStrategy extends ReactiveElement {
           link_dashboard: true,
         });
       }
+    }
+
+    const noAreaFilter = generateEntityFilter(hass, {
+      area: null,
+    });
+
+    const otherEntities = allEntities.filter(noAreaFilter);
+
+    if (otherEntities.length > 0) {
+      widgetSection.cards!.push({
+        type: "tile",
+        entity: otherEntities[0],
+        icon: "mdi:shape",
+        name: "Unassigned devices",
+        hide_state: true,
+        tap_action: {
+          action: "navigate",
+          navigation_path: "unassigned-devices",
+        },
+        icon_tap_action: {
+          action: "none",
+        },
+      });
     }
 
     const sections = (
